b04014a608abaa8390c2b3774fd86b9a7f655d2f
Merk42 <mark_c@markecurtis.com>
2025-12-08 12:46:46 -0500

    button states

--- src/app/components/button/button.ts
+++ src/app/components/button/button.ts
@@ -1,14 +1,55 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { HeroIcon } from '../hero-icon/hero-icon';
+type ButtonStatus = ''|'try'|'success'|'fail'
 
 @Component({
   selector: 'button[mec-button]',
-  imports: [],
+  imports: [HeroIcon],
   templateUrl: './button.html',
   host: {
-    'class': 'group cursor-pointer disabled:cursor-not-allowed grow text-center relative before:content-[\'\'] before:absolute before:inset-0 before:bg-yellow-800 before:rounded-md disabled:opacity-50'
+    'class': 'group cursor-pointer disabled:cursor-not-allowed grow text-center relative before:content-[\'\'] before:absolute before:inset-0 before:bg-yellow-800 before:rounded-md disabled:opacity-50',
+
   },
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Button {
 
+  // '[attr.disabled]': 'isDisabled() || isAnimating() || null'
+
+  isDisabled = signal<boolean>(false)
+  displayState = signal<ButtonStatus>('');
+  isAnimating = computed(() => {
+    return this.displayState() !== ''
+  })
+
+	private updateStatus(status: ButtonStatus, disabledafter: boolean) {
+		this.displayState.set(status);
+		if (status === 'fail' || status === 'success') {
+			this.resetStatus(disabledafter)
+		}
+	}
+
+	private resetStatus(disabledafter: boolean) {
+		setTimeout(() => {
+      console.log('reset after timeout')
+      this.displayState.set('');
+			if (disabledafter) {
+				this.isDisabled.set(true)
+			} else {
+				this.isDisabled.set(false)
+			}
+		}, 3000);
+	}
+
+	animateTry(disabledafter: boolean = false) {
+		this.updateStatus('try', disabledafter)
+	}
+
+	animateSuccess(disabledafter: boolean = false) {
+		this.updateStatus('success', disabledafter)
+	}
+
+	animateFail(disabledafter: boolean = false) {
+		this.updateStatus('fail', disabledafter)
+	}
 }
--- src/app/examples/button-example/button-example.ts
+++ src/app/examples/button-example/button-example.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, viewChild } from '@angular/core';
 import { Button } from "../../components/button/button";
 
 @Component({
@@ -8,5 +8,24 @@ import { Button } from "../../components/button/button";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ButtonExample {
+  example = viewChild.required(Button)
 
+  animate(t:string) {
+    console.log('animate', t)
+    if(t === 'try') {
+      this.example().animateTry()
+    }
+    if(t === 'success') {
+      this.example().animateTry()
+      setTimeout(() => {
+        this.example().animateSuccess()
+      }, 2000);
+    }
+    if(t === 'fail') {
+      this.example().animateTry()
+      setTimeout(() => {
+        this.example().animateFail()
+      }, 2000);
+    }
+  }
 }
