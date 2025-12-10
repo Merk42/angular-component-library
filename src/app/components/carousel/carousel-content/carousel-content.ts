f0e068e307b39af7681e2d1c8644ab5427854758
Merk42 <mark_c@markecurtis.com>
2025-12-10 10:45:51 -0500

    moving most to tailwind classes

--- src/app/components/carousel/carousel-content/carousel-content.ts
+++ src/app/components/carousel/carousel-content/carousel-content.ts
@@ -4,7 +4,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'mec-carousel-content',
   imports: [],
   templateUrl: './carousel-content.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: "relative flex px-2 items-start min-w-0 w-auto grow-0 shrink-0"
+  }
 })
 export class CarouselContent {
 
