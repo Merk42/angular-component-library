2774a0dafdc2a90c75806f3b1470e57668314a63
Merk42 <mark_c@markecurtis.com>
2026-06-07 10:14:59 -0400

    @Service

--- projects/mec-at/src/lib/accordion/accordion-service.ts
+++ projects/mec-at/src/lib/accordion/accordion-service.ts
@@ -1,9 +1,7 @@
-import { Injectable } from '@angular/core';
+import { Service } from '@angular/core';
 import { Subject } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Service()
 export class AccordionService {
 
   // Observable string sources
